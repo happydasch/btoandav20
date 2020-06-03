5801f8d6018ee35b5099dc18f3250725a13ff238
Daniel Schindler <daniel@vcard24.de>
2020-06-03 13:07:08 +0200

    Removed supressing of errors from imports

--- btoandav20/__init__.py
+++ btoandav20/__init__.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
 
 from . import feeds as feeds
 from . import stores as stores
--- btoandav20/feeds/__init__.py
+++ btoandav20/feeds/__init__.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-try:
-    from .oandav20feed import OandaV20Data
-except ImportError as e:
-    pass  # The user may not have something installed
+from .oandav20feed import OandaV20Data
