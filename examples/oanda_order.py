7c7aaeb1f3a5649f9b61d3cfca22afa4a950aedf
Daniel Schindler <daniel@vcard24.de>
2020-06-16 01:53:47 +0200

    added order execution examples

---
+++ examples/oanda_order.py
@@ -0,0 +1,157 @@
+import backtrader as bt
+import btoandav20 as bto
+import json
+
+''' Test for orders from oanda '''
+
+
+class St(bt.Strategy):
+
+    params = dict(
+        order_type=bt.Order.Stop,
+        use_brackets=True,
+        sell=False
+    )
+
+    def __init__(self):
+        self.order = None
+
+    def notify_store(self, msg, *args, **kwargs):
+        txt = ["*" * 5, "STORE NOTIF:", msg]
+        print(", ".join(txt))
+
+    def notify_order(self, order):
+        txt = ["*" * 5, "ORDER NOTIF:", str(order)]
+        print(", ".join(txt))
+
+    def notify_trade(self, trade):
+        txt = ["*" * 5, "TRADE NOTIF:", str(trade)]
+        print(", ".join(txt))
+
+    def next(self):
+
+        if self.order:
+            return
+
+        # create a market order
+        if self.p.order_type == bt.Order.Market:
+            price = self.data.close[0]
+            if self.p.use_brackets:
+                if self.p.sell:
+                    self.order, _, _ = self.sell_bracket(
+                        size=1, exectype=bt.Order.Market,
+                        oargs={},
+                        stopprice=price + 0.0002,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price - 0.0002,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+                else:
+                    self.order, _, _ = self.buy_bracket(
+                        size=1, exectype=bt.Order.Market,
+                        oargs={},
+                        stopprice=price - 0.0002,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price + 0.0002,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+            else:
+                if self.p.sell:
+                    self.order = self.sell(exectype=bt.Order.Market, size=1)
+                else:
+                    self.order = self.buy(exectype=bt.Order.Market, size=1)
+        elif self.p.order_type == bt.Order.Limit:
+            price = self.data.close[0]
+            if self.p.use_brackets:
+                if self.p.sell:
+                    self.order, _, _ = self.sell_bracket(
+                        size=1,
+                        exectype=bt.Order.Limit,
+                        price=price,
+                        oargs={},
+                        stopprice=price + 0.0005,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price - 0.0005,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+                else:
+                    self.order, _, _ = self.buy_bracket(
+                        size=1,
+                        exectype=bt.Order.Limit,
+                        price=price,
+                        oargs={},
+                        stopprice=price - 0.0005,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price + 0.0005,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+            else:
+                if self.p.sell:
+                    self.order = self.sell(exectype=bt.Order.Limit, price=price, size=1)
+                else:
+                    self.order = self.buy(exectype=bt.Order.Limit, price=price, size=1)
+        elif self.p.order_type == bt.Order.Stop:
+            price = self.data.close[0]
+            if self.p.use_brackets:
+                if self.p.sell:
+                    self.order, _, _ = self.sell_bracket(
+                        size=1,
+                        exectype=bt.Order.Stop,
+                        price=price,
+                        oargs={},
+                        stopprice=price + 0.0002,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price - 0.0002,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+                else:
+                    self.order, _, _ = self.buy_bracket(
+                        size=1,
+                        exectype=bt.Order.Stop,
+                        price=price,
+                        oargs={},
+                        stopprice=price - 0.0005,
+                        stopexec=bt.Order.Stop,
+                        stopargs={},
+                        limitprice=price + 0.0005,
+                        limitexec=bt.Order.Limit,
+                        limitargs={})
+            else:
+                if self.p.sell:
+                    self.order = self.sell(exectype=bt.Order.Stop, price=price, size=1)
+                else:
+                    self.order = self.buy(exectype=bt.Order.Stop, price=price, size=1)
+
+
+with open("config/config.json", "r") as file:
+    config = json.load(file)
+
+storekwargs = dict(
+    token=config["oanda"]["token"],
+    account=config["oanda"]["account"],
+    practice=config["oanda"]["practice"],
+    notif_transactions=True,
+    stream_timeout=10,
+)
+store = bto.stores.OandaV20Store(**storekwargs)
+datakwargs = dict(
+    timeframe=bt.TimeFrame.Seconds,
+    compression=30,
+    tz='Europe/Berlin',
+    backfill=False,
+    backfill_start=False,
+)
+data = store.getdata(dataname="EUR_USD", **datakwargs)
+data.resample(
+    timeframe=bt.TimeFrame.Seconds,
+    compression=30)  # rightedge=True, boundoff=1)
+cerebro = bt.Cerebro()
+cerebro.adddata(data)
+cerebro.setbroker(store.getbroker())
+cerebro.addstrategy(St)
+cerebro.run()
---
+++ examples/oanda_order_replace.py
@@ -0,0 +1,103 @@
+import backtrader as bt
+import btoandav20 as bto
+import json
+
+''' Test for orders from oanda '''
+
+
+class St(bt.Strategy):
+
+    params = {
+        "order_type": bt.Order.StopTrail,
+    }
+
+    def __init__(self):
+        self.order = None
+        self.stop = None
+        self.limit = None
+
+    def notify_store(self, msg, *args, **kwargs):
+        txt = ["*" * 5, "STORE NOTIF:", msg]
+        print(", ".join(txt))
+
+    def notify_order(self, order):
+        if order.status == order.Completed:
+            if order.ref == self.order.ref:
+                self.order = None
+        txt = ["*" * 5, "ORDER NOTIF:", str(order)]
+        print(", ".join(txt))
+
+    def notify_trade(self, trade):
+        txt = ["*" * 5, "TRADE NOTIF:", str(trade)]
+        print(", ".join(txt))
+
+    def next(self):
+
+        if self.order:
+            return
+
+        if self.p.order_type == bt.Order.StopLimit:
+            if not self.stop and not self.limit:
+                price = self.data_close[0] + 0.0002
+                self.order, self.stop, self.limit = self.buy_bracket(
+                    size=1,
+                    exectype=bt.Order.Stop,
+                    price=price,
+                    oargs={},
+                    stopexec=None,
+                    limitprice=price+0.001,
+                    limitexec=bt.Order.StopLimit,
+                    limitargs={})
+            elif self.limit:
+                price = self.data_close[0] + 0.002
+                self.limit = self.sell(
+                    exectype=bt.Order.StopLimit,
+                    plimit=price,
+                    replace=self.limit.ref)
+        elif self.p.order_type == bt.Order.StopTrail:
+            if not self.stop and not self.limit:
+                price = self.data_close[0] + 0.0002
+                self.order, self.stop, self.limit = self.buy_bracket(
+                    size=1,
+                    exectype=bt.Order.Stop,
+                    price=price,
+                    oargs={},
+                    stopexec=bt.Order.StopTrail,
+                    stopargs={
+                        "trailamount": 0.0005
+                    },
+                    limitexec=None,)
+            elif self.stop:
+                self.stop = self.sell(
+                    exectype=bt.Order.StopTrail,
+                    trailamount=0.001,
+                    replace=self.stop.ref)
+
+
+with open("config/config.json", "r") as file:
+    config = json.load(file)
+
+storekwargs = dict(
+    token=config["oanda"]["token"],
+    account=config["oanda"]["account"],
+    practice=config["oanda"]["practice"],
+    notif_transactions=True,
+    stream_timeout=10,
+)
+store = bto.stores.OandaV20Store(**storekwargs)
+datakwargs = dict(
+    timeframe=bt.TimeFrame.Minutes,
+    compression=1,
+    tz='Europe/Berlin',
+    backfill=False,
+    backfill_start=False,
+)
+data = store.getdata(dataname="EUR_USD", **datakwargs)
+data.resample(
+    timeframe=bt.TimeFrame.Minutes,
+    compression=1)  # rightedge=True, boundoff=1)
+cerebro = bt.Cerebro()
+cerebro.adddata(data)
+cerebro.setbroker(store.getbroker())
+cerebro.addstrategy(St)
+cerebro.run()
