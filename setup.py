a1c189e6b281c4f7ee950fd9b6efdefa805dc27b
Daniel Schindler <daniel@vcard24.de>
2020-06-16 00:01:01 +0200

    updated version to reflect latest changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="backtrader-oandav20",
-    version="0.1.0",
+    version="0.2.0",
     description="Integrate Oanda-V20 API into backtrader",
     long_description=long_description,
     license='GNU General Public License Version 3',
