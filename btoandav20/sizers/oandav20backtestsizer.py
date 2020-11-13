296e5fb4ca96ff72057c1a8531822522c8ccd563
Daniel Schindler <daniel@vcard24.de>
2020-11-13 15:44:59 +0100

    updated sizer for backtest

---
+++ btoandav20/sizers/oandav20backtestsizer.py
@@ -0,0 +1,65 @@
+import backtrader as bt
+
+from btoandav20.commissions import OandaV20BacktestCommInfo
+
+
+class OandaV20BacktestRiskSizer(bt.Sizer):
+
+    params = dict(
+        percents=0,   # risk percents
+        amount=0,     # risk amount
+        pips=5,   # stop loss in pips
+    )
+
+    def getsizing(self, data, isbuy, pips=None, price=None,
+                  exchange_rate=None):
+        comminfo = self.broker.getcommissioninfo(data)
+        return self._getsizing(
+            comminfo, self.broker.getvalue(),
+            data, isbuy, pips, price, exchange_rate)
+
+    def _getsizing(self, comminfo, cash, data, isbuy, pips=None,
+                   price=None, exchange_rate=None):
+        position = self.broker.getposition(data)
+        if position:
+            return position.size
+        if not pips:
+            pips = self.p.pips
+        cash = self.broker.getcash()
+        if self.p.percents != 0:
+            cash_to_use = cash * (self.p.percents/100)
+        elif self.p.amount != 0:
+            cash_to_use = self.p.amount
+        else:
+            raise Exception('Either percents or amount is needed')
+        if not isinstance(comminfo, OandaV20BacktestCommInfo):
+            raise Exception('OandaV20CommInfo required')
+
+        multiplier = float(10 ** comminfo.p.pip_location)
+        price_per_pip = cash_to_use / pips
+        if not comminfo.p.acc_counter_currency and price:
+            # Acc currency is same as base currency
+            pip_value = price_per_pip * price
+            size = pip_value / multiplier
+        elif exchange_rate:
+            # Acc currency is neither same as base or counter currency
+            pip_value = price_per_pip * exchange_rate
+            size = pip_value / multiplier
+        else:
+            # Acc currency and counter currency are the same
+            size = price_per_pip / multiplier
+        return int(size)
+
+
+class OandaV20BacktestRiskPercentSizer(OandaV20BacktestRiskSizer):
+
+    params = dict(
+        percents=5,
+    )
+
+
+class OandaV20BacktestRiskCashSizer(OandaV20BacktestRiskSizer):
+
+    params = dict(
+        amount=50,
+    )
