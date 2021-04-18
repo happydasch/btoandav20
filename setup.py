8b774b473f31f0a781bab25168cd5d23b557108f
Daniel Schindler <daniel@vcard24.de>
2021-04-18 14:31:37 +0200

    updated setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+from distutils.util import convert_path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+main_ns = {}
+ver_path = convert_path('btoandav20/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
 setuptools.setup(
     name="btoandav20",
-    version="0.2.0",
+    version=main_ns['__version__'],
     description="Integrate Oanda-V20 API into backtrader",
     long_description=long_description,
     license='GNU General Public License Version 3',
